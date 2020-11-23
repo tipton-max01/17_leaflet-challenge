9ea7b84725a7b26ad25301a4de760b6e11de1585
Max Tipton <bartholdy9000@tippyPro15.fios-router.home>
2020-11-23 02:17:50 -0500

    create map & darkmap layer

--- leaflet-step-1/static/js/logic.js
+++ leaflet-step-1/static/js/logic.js
@@ -1,11 +1,29 @@
-//Part 1 Get the data set
+//Part 1 Get the data set. 
 
-// Store our API endpoint as queryUrl
+//Load in the geojson data.
 var geoData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
 
-// Perform a GET request to the query URL
+// Grab data with d3
 d3.json(geoData, function(data) {
   console.log(data.features);
   // Using the features array sent back in the API data, create a GeoJSON layer and add it to the map
 
+});
+
+
+// Define darkmap layer
+var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "dark-v10",
+  accessToken: API_KEY
+});
+
+// Create a new map
+var myMap = L.map("map", {
+  center: [
+    37.09, -95.71
+  ],
+  zoom: 5,
+  layers: [darkmap]
 });
