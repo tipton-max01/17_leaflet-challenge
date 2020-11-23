f3faabada0cd77406a8fc4ad4616ef2a6eb95d94
Max Tipton <bartholdy9000@tippyPro15.fios-router.home>
2020-11-23 01:57:57 -0500

    acquired the geo data in an array

--- leaflet-step-1/static/js/logic.js
+++ leaflet-step-1/static/js/logic.js
@@ -1,10 +1,11 @@
 //Part 1 Get the data set
 
 // Store our API endpoint as queryUrl
-// var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
+var geoData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
 
 // Perform a GET request to the query URL
-d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson", function(response) { 
-  console.log(response.features);
+d3.json(geoData, function(data) {
+  console.log(data.features);
   // Using the features array sent back in the API data, create a GeoJSON layer and add it to the map
+
 });
