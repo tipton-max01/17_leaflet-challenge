5b98b85bb3ddd0681aaf5421acff38edeca1d86d
Max Tipton <bartholdy9000@tippyPro15.fios-router.home>
2020-11-23 02:23:54 -0500

    shifted logic with create map and adding layer

--- leaflet-step-1/static/js/logic.js
+++ leaflet-step-1/static/js/logic.js
@@ -1,3 +1,20 @@
+// Create a new map
+var myMap = L.map("map", {
+  center: [
+    37.09, -95.71
+  ],
+  zoom: 5,
+});
+
+// Define darkmap layer
+var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "dark-v10",
+  accessToken: API_KEY
+}).addTo(myMap);
+
+
 //Part 1 Get the data set. 
 
 //Load in the geojson data.
@@ -8,22 +25,4 @@ d3.json(geoData, function(data) {
   console.log(data.features);
   // Using the features array sent back in the API data, create a GeoJSON layer and add it to the map
 
-});
-
-
-// Define darkmap layer
-var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-  maxZoom: 18,
-  id: "dark-v10",
-  accessToken: API_KEY
-});
-
-// Create a new map
-var myMap = L.map("map", {
-  center: [
-    37.09, -95.71
-  ],
-  zoom: 5,
-  layers: [darkmap]
 });
