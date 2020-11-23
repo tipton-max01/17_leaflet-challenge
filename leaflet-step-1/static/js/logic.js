7a0440b25e47fdc1cc7b25b2e3f93e1aff6f2a36
Max Tipton <bartholdy9000@tippyPro15.fios-router.home>
2020-11-23 01:53:27 -0500

    finally defined d3 script in html

--- leaflet-step-1/static/js/logic.js
+++ leaflet-step-1/static/js/logic.js
@@ -1,8 +1,10 @@
 //Part 1 Get the data set
 
 // Store our API endpoint as queryUrl
-var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
+// var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
 
-// // Perform a GET request to the query URL
-// d3.json(queryUrl, function(data) {
-//   console.log(data.features);
+// Perform a GET request to the query URL
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson", function(response) { 
+  console.log(response.features);
+  // Using the features array sent back in the API data, create a GeoJSON layer and add it to the map
+});
