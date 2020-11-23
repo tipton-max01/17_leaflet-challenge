b807a263b145ca2f6aad0c7d938d9e7f912c4afd
Max Tipton <bartholdy9000@tippyPro15.fios-router.home>
2020-11-23 01:33:05 -0500

    solved static routes in html

--- leaflet-step-1/static/js/logic.js
+++ leaflet-step-1/static/js/logic.js
@@ -3,6 +3,6 @@
 // Store our API endpoint as queryUrl
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
 
-// Perform a GET request to the query URL
-d3.json(queryUrl, function(data) {
-  console.log(data.features);
+// // Perform a GET request to the query URL
+// d3.json(queryUrl, function(data) {
+//   console.log(data.features);
